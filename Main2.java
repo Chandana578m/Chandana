a18c86900b470a3a250b680190b5c091a2390faa
Chandana578m <chandanameruvaa@gmail.com>
2025-06-04 10:52:59 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Main2
+{
+public static void main(String[]args){
+String name="hello";
+System.out.println("Sting name="+name);
+String newString=new String("hello");
+System.out.println("String newString="+newString);
+}
+}
---
+++ Main3.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class Main3{
+public static void main(String[] args){
+String name="Sachin";
+name=name.concat("Tendulkar");
+System.out.println(name);
+}
+}
